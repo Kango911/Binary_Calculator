c65591da3a3c165461cdf172b4014312a45d2bf8
Kango911 <lipenkov.a61@gmail.com>
2022-12-17 12:49:13 +0500

    Add NegativeValue

---
+++ Binary_Calculator/NegativeValue.cs
@@ -0,0 +1,14 @@
+﻿namespace binary_calculator
+{
+    public class NegativeValue
+    {
+        public static (string,bool) AdditionalCode(int number, int length)
+        {
+            string firstBin = TranslationIntoSystems.ToBin(number);
+            firstBin = BinaryOperation.Inverse(firstBin);
+            firstBin = BinaryOperation.AddNonSignPoint(firstBin,length);
+            firstBin = BinaryOperation.Plus(firstBin, "1");
+            return (firstBin,true);
+        }
+    }
+}
